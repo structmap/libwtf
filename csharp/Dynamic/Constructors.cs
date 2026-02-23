bdaa27a4d75b8f6e4ae95dbba1d6840ba6719694
James Davidson <jamesd3142@gmail.com>
2026-02-23 13:02:15 +0800

    Need to handle union type

--- csharp/Dynamic/Constructors.cs
+++ csharp/Dynamic/Constructors.cs
@@ -6,14 +6,34 @@ namespace Structmap.WebTransportFast.Dynamic;
 [QuickConstructor(Fields=IncludeFields.AllFields)]
 public partial struct wtf_certificate_config_t
 {
-    [QuickConstructor(Fields=IncludeFields.AllFields)]
     public partial struct _cert_data_e__Union
     {
-        [QuickConstructor(Fields=IncludeFields.AllFields)] public partial struct _file_e__Struct;
-        [QuickConstructor(Fields=IncludeFields.AllFields)] public partial struct _protected_file_e__Struct;
-        [QuickConstructor(Fields=IncludeFields.AllFields)] public partial struct _hash_e__Struct;
-        [QuickConstructor(Fields=IncludeFields.AllFields)] public partial struct _hash_store_e__Struct;
-        [QuickConstructor(Fields=IncludeFields.AllFields)] public partial struct _pkcs12_e__Struct;
+        public _cert_data_e__Union(_file_e__Struct fileEStruct)
+        {
+            file = fileEStruct;
+        }
+        [QuickConstructor(Fields = IncludeFields.AllFields)]
+        public partial struct _file_e__Struct;
+        public _cert_data_e__Union(_protected_file_e__Struct protected_file_e__Struct) {
+            protected_file = protected_file_e__Struct;
+        }
+        [QuickConstructor(Fields = IncludeFields.AllFields)]
+        public partial struct _protected_file_e__Struct;
+        public _cert_data_e__Union(_hash_e__Struct hash_e__Struct) {
+            hash = hash_e__Struct;
+        }
+        [QuickConstructor(Fields = IncludeFields.AllFields)]
+        public partial struct _hash_e__Struct;
+        public _cert_data_e__Union(_hash_store_e__Struct hash_store_e__Struct) {
+            hash_store = hash_store_e__Struct;
+        }
+        [QuickConstructor(Fields = IncludeFields.AllFields)]
+        public partial struct _hash_store_e__Struct;
+        public _cert_data_e__Union(_pkcs12_e__Struct pkcs12_e__Struct) {
+            pkcs12 = pkcs12_e__Struct;
+        }
+        [QuickConstructor(Fields = IncludeFields.AllFields)]
+        public partial struct _pkcs12_e__Struct;
     }
 };
 [QuickConstructor(Fields=IncludeFields.AllFields)] public partial struct wtf_server_config_t;

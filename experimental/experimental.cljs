5470a83349c77aa3f43516400f2e196ff19b92b8
James Davidson <jamesd3142@gmail.com>
2026-03-25 15:54:47 +0800

    Update test code

--- experimental/experimental.cljs
+++ experimental/experimental.cljs
@@ -29,7 +29,7 @@
     (try
       (loop []
         (let [{value "value" done "done"} (js->clj (<p! (.read r)))]
-          (js/alert (.decode d value))
+          (js/console.log (.decode d value))
           (when-not done (recur))))
       (catch js/Error err (js/console.log (ex-cause err))))))
 
@@ -38,21 +38,29 @@
     (let [d (new js/TextDecoderStream "utf-8")
           r (-> readable (.pipeThrough d) .getReader)]
       (try
+        ;(<p! (.-ready readable))
         (loop []
           (let [{value "value" done "done"} (js->clj (<p! (.read r)))]
-            (js/console.log value)
+            (if (nil? value)
+              (js/console.log "nil value in stream reader")
+              (js/console.log value))
             (when-not done (recur))))
         (catch js/Error err (js/console.log (ex-cause err)))))))
 
-(go
-  (let [enc (new js/TextEncoder "utf-8")
-        data (.encode enc "hello world")
-        s (<p! (.createBidirectionalStream wt))
-        w (some-> s .-writable .getWriter)]
-    (try
-      (log-stream-data (.-readable s))
-      (<p! (.write w data))
-      (<p! (.close w))
-      (catch js/Error err (js/console.log (ex-cause err))))))
+(def enc (new js/TextEncoder "utf-8"))
+(def data (.encode enc "hello world"))
+
+;; wait
+(go (def s (<p! (.createBidirectionalStream wt))))
+
+;; wait
+(log-stream-data (.-readable s))
+(def w (some-> s .-writable .getWriter))
+
+(go (println (<p! (.write w data))))
+
+(.write w data)
+
+(.close w)
 
 )
